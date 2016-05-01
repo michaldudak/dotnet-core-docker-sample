8f34fa4a564ec984f87d119ade7dce29a7898411
Michał Dudak <michal.dudak@gmail.com>
2016-05-01 21:13:23 +0200

    Added environment info page

--- Startup.cs
+++ Startup.cs
@@ -2,16 +2,20 @@ using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace NetCoreWebApp
 {
     public class Startup
     {
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(LogLevel.Debug);
+            app.UseRuntimeInfoPage("/env");
             app.Run(async context =>
             {
-                await context.Response.WriteAsync("Hello World!\n");
+                context.Response.ContentType = "text/plain";
+                await context.Response.WriteAsync("Hello World!\r\n");
                 await context.Response.WriteAsync($"This is {Environment.MachineName}.");
             });
         }
